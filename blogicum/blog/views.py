0619f799ba9a005812783c00b3eba23df9ff7e46
Роман Кондрашов <roman.condrashov200015@gmail.com>
2024-09-26 19:40:11 +0300

    fix

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,11 +1,13 @@
 from django.http import Http404
 from django.shortcuts import render
 from blog.models import Post, Category
-from django.utils.timezone import now
+from datetime import datetime
 
 
 def index(request):
-    post_list = Post.objects.all().filter(is_published=True, category__is_published=True, pub_date__lt=now())[:5]
+    post_list = Post.objects.all().filter(is_published=True,
+                                          category__is_published=True,
+                                          pub_date__lt=datetime.now())[:5]
     context = {
         'post_list': post_list,
     }
@@ -17,17 +19,23 @@ def post_detail(request, post_id):
         post = Post.objects.get(id=post_id)
     except KeyError:
         raise Http404(f"Пост {post_id} не найден")
-    if post.pub_date > datetime.now() or not post.is_published or not post.category.is_published:
-        raise Http404(f"Пост не доступен")
-    context = {'post': post,}
+    if (
+        post.pub_date > datetime.now() or
+        not post.is_published or
+        (post.category and not post.category.is_published)
+    ):
+        raise Http404("Пост не доступен")
+    context = {'post': post, }
     return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug):
-    category = Category.objects.get(slug = category_slug)
+    category = Category.objects.get(slug=category_slug)
     if not category.is_published:
         raise Http404("Категория не опубликована.")
-    post_list = Post.objects.all().filter(category=category, is_published=True, pub_date__lte=datetime.now())
+    post_list = Post.objects.all().filter(category=category,
+                                          is_published=True,
+                                          pub_date__lte=datetime.now())
     context = {
         'category': category,
         'post_list': post_list
